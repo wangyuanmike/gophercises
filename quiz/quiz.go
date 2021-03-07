3ac575443ac2b99831be2810b21a055cd77f82ad
Yuan Wang <mike.wang.1203@gmail.com>
2021-03-07 12:00:23 +0100

    add csv reader and std reader to quiz.go

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,1 +1,32 @@
-test
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("problems.csv")
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvReader := csv.NewReader(bufio.NewReader(f))
+	stdReader := bufio.NewReader(os.Stdin)
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record[0])
+		stdReader.ReadLine()
+	}
+}
