2166202ca0307689fa03a7ab9d0c8748d4c8f3dc
Yuan Wang <mike.wang.1203@gmail.com>
2021-03-08 11:35:42 +0100

    finish part 1 of quiz exercise

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,32 +1,60 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"fmt"
-	"io"
-	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	f, err := os.Open("problems.csv")
+type problem struct {
+	question string
+	answer   string
+}
+
+func exit(errMsg string) {
+	fmt.Println(errMsg)
+	os.Exit(1)
+}
+
+func parseProblem(problemFile string) []problem {
+	f, err := os.Open(problemFile)
 	defer f.Close()
 	if err != nil {
-		log.Fatal(err)
+		exit("Cannot open problem file...")
 	}
 
-	csvReader := csv.NewReader(bufio.NewReader(f))
-	stdReader := bufio.NewReader(os.Stdin)
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(record[0])
-		stdReader.ReadLine()
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		exit("Cannot read problem file...")
 	}
+
+	var problems = make([]problem, len(records))
+	for i := range records {
+		problems[i].question = records[i][0]
+		problems[i].answer = strings.TrimSpace(records[i][1])
+	}
+	return problems
+}
+
+func executeQuiz(problemFile string) {
+	problems := parseProblem(problemFile)
+
+	correctCount := 0
+	var answer string
+	fmt.Println("Quiz starts...")
+	for i := range problems {
+		fmt.Println(problems[i].question)
+		fmt.Scanln(&answer)
+		if answer == problems[i].answer {
+			correctCount++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d questions", correctCount, len(problems))
+}
+
+func main() {
+	executeQuiz("problems.csv")
 }
